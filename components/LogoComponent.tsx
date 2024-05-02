48d8943f6de3fd0159b1ad99411ebc977ab14a07
Ian Sebugenyi <140151921+Iancustor@users.noreply.github.com>
2024-05-02 17:29:08 +0300

    first commit on boogie template

---
+++ components/LogoComponent.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function LogoComponent() {
+  return (
+    <Link href="/" className="flex items-center gap-3">
+      <Image
+        width={1080}
+        height={1080}
+        src="/images/bogiel.png"
+        className=" w-8 h-8 rounded-full object-cover"
+        alt=""
+      />
+      <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+        Boogie
+      </span>
+    </Link>
+  );
+}
+
+export default LogoComponent;
